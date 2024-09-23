301cd22edfa9fba28e168ec6ef9ff7ede6753d59
Guybson Lopes <guybsonlopescomercial@gmail.com>
2024-09-23 10:00:14 -0300

    Criando novo repositório

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Realizando configurações da página
+st.set_page_config(page_title='Dashboard', page_icon='content/dashboard_icon.png')
+
+st.image('content/dashboard_icon.png')
